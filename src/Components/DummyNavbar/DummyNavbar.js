9450da5bce6c96cf701591a06382e3bf1b16b41b
ChiragDugar <c.dugar2011@gmail.com>
2020-10-18 12:51:32 +0530

    Navbar Update

--- src/Components/DummyNavbar/DummyNavbar.js
+++ src/Components/DummyNavbar/DummyNavbar.js
@@ -1,49 +1,58 @@
 import React from "react";
 import { Navbar } from "react-bootstrap";
 import logo from "../../assets/logo.png";
+import "./DummyNav.css";
 
 function DummyNavbar() {
   return (
-    <div style={{ backgroundColor: "#11151B" }}>
-      <Navbar className="container" style={{ position: "sticky" }}>
-        <Navbar.Brand href="/" style={{ color: "#EFF3F8" }}>
-          <img src={logo} alt="logo"></img>
-        </Navbar.Brand>
-        <Navbar.Toggle />
-        <Navbar.Collapse className="justify-content-end">
-          <Navbar.Text className="mr-4">
-            <a
-              href="https://github.com/ChiragDugar/VITHack2020_Vicara"
-              target="_blank"
-              rel="noopener noreferrer"
-              style={{
-                color: "#EFF3F8",
-                fontWeight: 600,
-                fontSize: 14,
-                letterSpacing: "0.3em",
-              }}
-            >
-              FAQ
-            </a>
-          </Navbar.Text>
-          <Navbar.Text>
-            <a
-              href="https://www.canva.com/design/DAEKOh14oEM/y-owL5s4jFKPCRtAfvMGiw/view"
-              target="_blank"
-              rel="noopener noreferrer"
-              style={{
-                color: "#EFF3F8",
-                fontWeight: 600,
-                fontSize: 14,
-                letterSpacing: "0.3em",
-              }}
-            >
-              ABOUT
-            </a>
-          </Navbar.Text>
-        </Navbar.Collapse>
-      </Navbar>
-    </div>
+    // <div  style={{ backgroundColor: '#11151B',width:'100vw' }} >
+    <Navbar
+      className="container navwidth"
+      style={{
+        position: "fixed",
+        backgroundColor: "rgb(17, 21, 27)",
+        zIndex: 9999,
+        width: "100%",
+      }}
+    >
+      <Navbar.Brand href="/" style={{ color: "#EFF3F8" }}>
+        <img src={logo} alt="logo"></img>
+      </Navbar.Brand>
+      <Navbar.Toggle />
+      <Navbar.Collapse className="justify-content-end mr-5">
+        <Navbar.Text className="mr-4">
+          <a
+            href="https://github.com/ChiragDugar/vitalizer"
+            target="_blank"
+            rel="noopener noreferrer"
+            style={{
+              color: "#EFF3F8",
+              fontWeight: 600,
+              fontSize: 14,
+              letterSpacing: "0.3em",
+            }}
+          >
+            FAQ
+          </a>
+        </Navbar.Text>
+        <Navbar.Text>
+          <a
+            href="https://www.canva.com/design/DAEKOh14oEM/y-owL5s4jFKPCRtAfvMGiw/view"
+            target="_blank"
+            rel="noopener noreferrer"
+            style={{
+              color: "#EFF3F8",
+              fontWeight: 600,
+              fontSize: 14,
+              letterSpacing: "0.3em",
+            }}
+          >
+            ABOUT
+          </a>
+        </Navbar.Text>
+      </Navbar.Collapse>
+    </Navbar>
+    // </div>
   );
 }
 
